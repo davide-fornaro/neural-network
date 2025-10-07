53cb9983b0f38bab5ca75fe9d676bfe641109bfa
Davide <davide.fornaro@hotmail.it>
2025-10-08 00:01:31 +0200

    updated terminal config

--- run.bat
+++ run.bat
@@ -1,2 +1,2 @@
 cls
-python main.py --optimizer AdamW --dataset cifar10 --epochs 50 --batch-size 128 --lr 0.001 --dropout 0.1
+python main.py --optimizer AdamW --dataset cifar10 --epochs 50 --batch-size 128 --lr 0.001 --dropout 0.01 --hidden 128 64 32
